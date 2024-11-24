8878df736740c7af10e22b3667d955307600ae4e
SudheerKumar18 <sudheerkumarmeejuru@gmail.com>
2024-11-24 21:44:02 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import HttpResponse
+def behra(request):
+    return HttpResponse('behra is good person and also is good human beiang') 
